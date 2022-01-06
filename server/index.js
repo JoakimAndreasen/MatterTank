ea3ced0416554ca432e755616c099e7deb330180
Jogge <61437971+JoakimAndreasen@users.noreply.github.com>
2022-01-06 10:21:57 +0100

    Boilerplate code for server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+//Setup express server to listen on port 3000
+var express = require("express");
+var app = express();
+var server = app.listen(3000);
+app.use(express.static("./client"));
+
+console.log("Server is running!");
+
+//start socket io and run "newConnection" for every new client
+var socket = require("socket.io");
+var io = socket(server);
+io.sockets.on("connection", newConnection);
