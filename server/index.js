81248727a8af28322caa6c3bfb63e5ed0865dadd
Jogge <61437971+JoakimAndreasen@users.noreply.github.com>
2022-01-10 19:43:01 +0100

    Prepared for Socket.io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-//Libaries
-var matter = require("matter-js");
+//Import helper functions
+const { createLobby, joinLobby, leaveLobby, switchLobby } = require("./lobby.js");
 
 //Setup express server to listen on port 3000
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 var server = app.listen(3000);
 app.use(express.static("./client"));
 
@@ -13,3 +13,30 @@ console.log("Server is running!");
 var socket = require("socket.io");
 var io = socket(server);
 io.sockets.on("connection", newConnection);
+
+let lobbys = [];
+
+io.of("/").adapter.on("create-room", (room) => {
+    //createLobby(room);
+});
+
+io.of("/").adapter.on("create-room", (room) => {
+    //createLobby(room);
+});
+
+io.of("/").adapter.on("join-room", (room, id) => {
+    //joinLobby(room, id);
+});
+
+io.of("/").adapter.on("leave-room", (room, id) => {
+    //leaveLobby(room, id);
+});
+
+function newConnection(socket) {
+    let lobbyNumber;
+    let username = "";
+    socket.on("switchLobby", switchLobby);
+    socket.on("createLobby", createLobby);
+
+    console.log("New connection: ", socket.id);
+}
