583ee68be9e704dfd4a93ef32fba4ae27d87b69b
Jogge <61437971+JoakimAndreasen@users.noreply.github.com>
2022-02-03 15:00:06 +0100

    yes

---
+++ client/js/themes.js
@@ -0,0 +1,48 @@
+let themes = {
+    dark: {
+        background: "#121212",
+        title: "#",
+
+        game: {
+            background: "#",
+            border: "#"
+        },
+        card: {
+            background: "#",
+            button: "#",
+            text: "#",
+            title: "#"
+        }
+    },
+
+    light: {
+        background: "#",
+        title: "#",
+        game: {
+            background: "#",
+            border: "#"
+        },
+        card: {
+            background: "#",
+            button: "#",
+            text: "#",
+            title: "#"
+        }
+    },
+
+    colorful: {
+        background: "#",
+        title: "#",
+        game: {
+            background: "#",
+            border: "#"
+        },
+        card: {
+            background: "#",
+            button: "#",
+            text: "#",
+            title: "#"
+        }
+    }
+};
+
