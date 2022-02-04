c0ce529a128d3f35692d09a154ece682274b850f
davidfeldner <davidfeldner12@gmail.com>
2022-02-04 12:37:18 +0100

    BETTER css

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,4 +1,4 @@
-const messageContainer = document.getElementById('send-container')
+const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
